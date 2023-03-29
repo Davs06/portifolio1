9e83a6654563c9e3d09a67576ebe87696917e374
Davs06 <santosdv40@gmail.com>
2023-03-28 21:22:56 -0300

    portifolio protno

---
+++ index.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ reset:true})
+
+sr.reveal('.hide ', {duration:7000})
+sr.reveal('.hide1 ', {duration:5000})
+sr.reveal('.hide2 ', {duration:3000})
+sr.reveal('.hide3 ', {duration:1000})
