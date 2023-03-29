e512a4cbf8bb91987f15bbfdea6eaa9744d50252
Davs06 <santosdv40@gmail.com>
2023-03-29 15:51:50 -0300

    add whatsapp footer+alguns ajustes de responsivida

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.sr = ScrollReveal({ reset:true})
 
-sr.reveal('.hide ', {duration:7000})
-sr.reveal('.hide1 ', {duration:5000})
-sr.reveal('.hide2 ', {duration:3000})
-sr.reveal('.hide3 ', {duration:1000})
+sr.reveal('.hide ', {duration:4000})
+sr.reveal('.hide1 ', {duration:2000})
+sr.reveal('.hide2 ', {duration:800})
+sr.reveal('.hide3 ', {duration:600})
